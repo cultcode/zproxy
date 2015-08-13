cfcf8a253ae6358393fbc533a6f2bef55a08698d
Wei Luo <rovluo@gmail.com>
2015-08-13 17:35:21 +0800

    init

--- monitor_deli.sh
+++ monitor_deli.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-while true; do if ! pgrep -f deliMastSvr.py; then curl -sS -X POST 127.0.0.1:7070/ZkAgentSvr/ReleaseDeliMaster; fi; sleep 1; done
+while true; do if ! pgrep -f deliMastSvr.py; then curl -sS -X POST 127.0.0.1:7070/ZkAgentSvr/ReleaseDeliMaster; echo; fi; sleep 1; done
