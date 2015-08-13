f41b541029e8a9a4c04dfa65beb6c0239699dd36
Wei Luo <rovluo@gmail.com>
2015-08-13 15:06:07 +0800

    init

--- monitor_p2p.sh
+++ monitor_p2p.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-while true; do if ! pgrep -f P2POrgSvr.exe; then curl -X POST 127.0.0.1:7070/ZkAgentSvr/PayloadReport -d '{"TaskSum":9223372036854775807}'; fi; sleep 1; done
+while true; do if ! pgrep -f P2POrgSvr.exe; then curl -sS -X POST 127.0.0.1:7070/ZkAgentSvr/PayloadReport -d '{"TaskSum":9223372036854775807}'; fi; sleep 1; done
