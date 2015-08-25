35f973ef5c9df558ed107201e04afcd824d4ef91
Wei Luo <rovluo@gmail.com>
2015-08-25 16:04:22 +0800

    delay 3 seconds after pkill

--- run.deli.sh
+++ run.deli.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pkill -f zproxy
 pkill -f monitor_deli.sh
+sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >& /home/Titan/zproxy/zproxy.sh.log &
 nohup /home/Titan/zproxy/monitor_deli.sh >& /home/Titan/zproxy/monitor_deli.sh.log &
 pgrep -lf zproxy
--- run.p2p.sh
+++ run.p2p.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pkill -f zproxy
 pkill -f monitor_p2p.sh
+sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >& /home/Titan/zproxy/zproxy.sh.log &
 nohup /home/Titan/zproxy/monitor_p2p.sh >& /home/Titan/zproxy/monitor_p2p.sh.log &
 pgrep -lf zproxy
