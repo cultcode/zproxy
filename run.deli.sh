6f462abd0ff224d2df719ca232cd839311a747c9
Wei Luo <rovluo@gmail.com>
2015-08-26 17:41:36 +0800

    reverse the order of monitor and run

--- run.deli.sh
+++ run.deli.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pkill -f zproxy
 pkill -f monitor_deli.sh
+pkill -f zproxy
 sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >& /home/Titan/zproxy/zproxy.sh.log &
 nohup /home/Titan/zproxy/monitor_deli.sh >& /home/Titan/zproxy/monitor_deli.sh.log &
--- run.p2p.sh
+++ run.p2p.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pkill -f zproxy
 pkill -f monitor_p2p.sh
+pkill -f zproxy
 sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >& /home/Titan/zproxy/zproxy.sh.log &
 nohup /home/Titan/zproxy/monitor_p2p.sh >& /home/Titan/zproxy/monitor_p2p.sh.log &
