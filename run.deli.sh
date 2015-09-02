b3f24c217ee9d86319f9485b362c169e0bdc2fae
Wei Luo <rovluo@gmail.com>
2015-09-02 11:56:20 +0800

    kill zproxy.sh at first and kill zproxy.py sendcondly lest zorpxy.sh restart zproxy.py after zproxy.py killed

--- run.deli.sh
+++ run.deli.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 pkill -f monitor_deli.sh
-pkill -f zproxy
+pkill -f zproxy.sh
+pkill -f zproxy.py
 sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >> /home/Titan/zproxy/zproxy.sh.log 2>&1 &
 nohup /home/Titan/zproxy/monitor_deli.sh >> /home/Titan/zproxy/monitor_deli.sh.log 2>&1 &
 pgrep -lf zproxy
-pgrep -lf monitor_deli.sh
--- run.p2p.sh
+++ run.p2p.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 pkill -f monitor_p2p.sh
-pkill -f zproxy
+pkill -f zproxy.sh
+pkill -f zproxy.py
 sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >> /home/Titan/zproxy/zproxy.sh.log 2>&1 &
 nohup /home/Titan/zproxy/monitor_p2p.sh >> /home/Titan/zproxy/monitor_p2p.sh.log 2>&1 &
 pgrep -lf zproxy
-pgrep -lf monitor_p2p.sh
