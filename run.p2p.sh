fb8818444f7bf36709ca668aa4a2918f18d14e86
Rovere <rovluo@gmail.com>
2015-09-07 22:35:02 +0800

    Update run.p2p.sh
    wait 3seconds before grep

--- run.p2p.sh
+++ run.p2p.sh
@@ -2,7 +2,7 @@
 pkill -f monitor_p2p.sh
 pkill -f zproxy.sh
 pkill -f zproxy.py
-sleep 3
 nohup /home/Titan/zproxy/zproxy.sh >> /home/Titan/zproxy/zproxy.sh.log 2>&1 &
 nohup /home/Titan/zproxy/monitor_p2p.sh >> /home/Titan/zproxy/monitor_p2p.sh.log 2>&1 &
+sleep 3
 pgrep -lf zproxy
